7549bb8407a32380c61503d14f9f6a8af5de1a19
93Fahrenheit <farah.elabbouni@gmail.com>
2025-08-14 10:57:57 +0200

    bidouwa

--- Solutions/C04/ex02/ft_putnbr.c
+++ Solutions/C04/ex02/ft_putnbr.c
@@ -7,8 +7,8 @@ void	ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
-	if (nb == 2147483647)
-		write (1, "-2147483647", 11);
+	if (nb == 2147483648)
+		write (1, "-2147483648", 11);
 	else if (nb < 0)
 	{	
 		write(1, "-", 1);
---
+++ Solutions/C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,112 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/*int	check_base(char *base)
+{
+	int	i = 0;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[j] == base[i])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}*/
+
+
+int	check_base(char *base)
+{
+	int	i = 0;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' ||
+			base[i] == ' ' || (base[i] >= 9 && base[i] <= 13))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[j] == base[i])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long	nb;
+	int	base_len;
+
+	if (!check_base(base))
+		return ;
+	base_len = ft_strlen(base);
+	nb = (long)nbr;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= base_len)
+		ft_putnbr_base(nb / base_len, base);
+	ft_putchar(base[nb % base_len]);
+}
+
+int	main(void)
+{
+	// base 10 
+	ft_putnbr_base(12345, "0123456789"); // 12345
+	write(1, "\n", 1);
+
+	// négatif en base 10
+	ft_putnbr_base(-12345, "0123456789"); // -12345
+	write(1, "\n", 1);
+
+	// base binaire
+	ft_putnbr_base(42, "01"); // 101010
+	write(1, "\n", 1);
+
+	// base hexa
+	ft_putnbr_base(255, "0123456789ABCDEF"); // FF
+	write(1, "\n", 1);
+
+	// base octale
+	ft_putnbr_base(42, "poneyvif"); // yn
+	write(1, "\n", 1);
+
+	// base invalide (doublon)
+	ft_putnbr_base(42, "001234"); //
+	write(1, "\n", 1);
+
+	return (0);
+}
+
